85098d1b6370fb1255af5221904ba6a350f746a8
Monica Solines <160787107+monicasolines@users.noreply.github.com>
2024-09-05 14:02:23 +0000

    final

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extensions = [".com", ".net", ".us", ".io"];
+
+  let html = ``;
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let l = 0; l < noun.length; l++) {
+        for (let m = 0; m < extensions.length; m++) {
+          let domain = pronoun[i] + adj[j] + noun[l] + extensions[m];
+          html += `
+            <li class="list-group-item"> ${domain} </li>
+          `;
+        }
+      }
+    }
+  }
+
+  document.querySelector("#domainList").innerHTML = html;
 };
